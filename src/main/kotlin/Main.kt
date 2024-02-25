cc29c0261c8101eed9a053accfc989a9c03c1089
onkornix <dymyers@icloud.com>
2024-02-25 14:21:16 -0800

    100% finished. Output starts at highest value and decreases.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,11 +4,56 @@ import java.nio.file.Path
 
 
 fun main() {
-    //print("complete path to file: ")
 
-    val reader = File("/home/dylan/Documents/tfotr.txt").bufferedReader(charset = Charsets.ISO_8859_1, bufferSize = 4_000)
+    Files.deleteIfExists(Path.of("/home/dylan/Documents/output.txt"))
+    Files.createFile(Path.of("/home/dylan/Documents/output.txt"))
 
-    val wordsMap: MutableMap<String, Int> = mutableMapOf()
+    val wordsMap: MutableMap<String, Int> = createWordsMap()
+
+    val output = File("/home/dylan/Documents/output.txt")
+    val writer = output.bufferedWriter()
+
+    val minimumOccurrenceValue = wordsMap.values.minOf { it }
+    var currentOccurrenceValue = wordsMap.values.maxOf { it } - 1
+    val valuesThatExist: MutableList<Int> = getValuesThatExist(wordsMap)
+    var index = valuesThatExist.size
+
+    while (currentOccurrenceValue >= minimumOccurrenceValue) {
+
+        val indexList: MutableList<String> = mutableListOf()
+
+        for (word in wordsMap.keys) {
+            val value = wordsMap.getValue(word)
+            if (currentOccurrenceValue in valuesThatExist && value == currentOccurrenceValue) {
+                indexList.add(word)
+            }
+        }
+
+        if (indexList.isNotEmpty()) {
+
+            writer.write("\n$currentOccurrenceValue ----- \n${indexList.joinToString("\n")}")
+            writer.newLine()
+            writer.flush()
+        }
+
+        if (index == 0) {
+            break
+        }
+
+        index--
+        currentOccurrenceValue = valuesThatExist[index]
+
+    }
+    writer.close()
+}
+
+fun createWordsMap() : MutableMap<String,Int>{
+
+    print("Complete path to text file: ")
+    val input = readln()
+
+    val reader = File(input).bufferedReader()
+    val returnMap: MutableMap<String,Int> = mutableMapOf()
 
     reader.forEachLine {
         val wordList: MutableList<String> = mutableListOf()
@@ -23,59 +68,14 @@ fun main() {
             }
         }
         for (word in wordList) {
-            if (word in wordsMap.keys) {
-                wordsMap[word] = (wordsMap.getValue(word) + 1)
+            if (word in returnMap.keys) {
+                returnMap[word] = (returnMap.getValue(word) + 1)
             }
-            wordsMap.putIfAbsent(word, 1)
+            returnMap.putIfAbsent(word, 1)
 
         }
     }
-
-
-    Files.deleteIfExists(Path.of("/home/dylan/Documents/output.txt"))
-    Files.createFile(Path.of("/home/dylan/Documents/output.txt"))
-
-    val output = File("/home/dylan/Documents/output.txt")
-    val writer = output.bufferedWriter(Charsets.ISO_8859_1,4_000)
-
-
-    val max = wordsMap.values.maxOf { it }
-    var current = 1
-    val valuesThatExist: MutableList<Int> = mutableListOf()
-    var index = 0
-
-    for (value in wordsMap.values.sorted()) {
-        if (value !in valuesThatExist && value >= current) {
-            valuesThatExist.add(value)
-        }
-    }
-
-    while (current <= max) {
-
-        val indexList: MutableList<String> = mutableListOf()
-
-        for (word in wordsMap.keys) {
-            val value = wordsMap.getValue(word)
-            if (current in valuesThatExist && value == current) {
-                indexList.add(word)
-            }
-        }
-
-        if (indexList.isNotEmpty()) {
-            writer.write("\n$current ----- \n${indexList.joinToString("\n")}")
-            writer.flush()
-        }
-
-        if (index == valuesThatExist.size) {
-            break
-        }
-        current = valuesThatExist[index]
-        index++
-
-
-    }
-    writer.close()
-
+    return returnMap
 }
 
 fun removeSpecials(word: String): String {
@@ -84,5 +84,16 @@ fun removeSpecials(word: String): String {
     return word.filter {
         it !in specials
     }
+
+}
+fun getValuesThatExist(map:MutableMap<String,Int>): MutableList<Int> {
+
+    val returnList: MutableList<Int> = mutableListOf()
+    for (value in map.values.sorted()) {
+        if (value !in returnList) {
+            returnList.add(value)
+        }
+    }
+    return returnList
 
 }
