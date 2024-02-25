c1cb3f6cd0567301ea75dcde0e8a82c716d9f78a
onkornix <dymyers@icloud.com>
2024-02-25 13:05:23 -0800

    it's working! input, output, less than 1 second with the lord of the rings. - to do: make it go backwards

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,7 +6,7 @@ import java.nio.file.Path
 fun main() {
     //print("complete path to file: ")
 
-    val reader = File("/Users/4jStudent/Documents/readme.txt").bufferedReader(charset = Charsets.UTF_8, bufferSize = 4_000)
+    val reader = File("/home/dylan/Documents/tfotr.txt").bufferedReader(charset = Charsets.ISO_8859_1, bufferSize = 4_000)
 
     val wordsMap: MutableMap<String, Int> = mutableMapOf()
 
@@ -15,9 +15,7 @@ fun main() {
         val charList: MutableList<Char> = mutableListOf()
 
         for (char in it) {
-
             if (char.isWhitespace()) {
-                charList.add(char)
                 wordList.add(removeSpecials(charList.joinToString("").lowercase()))
                 charList.clear()
             } else {
@@ -33,52 +31,56 @@ fun main() {
         }
     }
 
-    println(makeOrderedMap(wordsMap))
 
-    /* FILE OUTPUT TEMPORARILY DISABLED
-    Files.deleteIfExists(Path.of("/Users/4JStudent/Documents/output.txt"))
-    Files.createFile(Path.of("/Users/4JStudent/Documents/output.txt"))
+    Files.deleteIfExists(Path.of("/home/dylan/Documents/output.txt"))
+    Files.createFile(Path.of("/home/dylan/Documents/output.txt"))
 
-    val output = File("/Users/4JStudent/Documents/output.txt")
-    val writer = output.bufferedWriter(Charsets.UTF_8,4_000)
+    val output = File("/home/dylan/Documents/output.txt")
+    val writer = output.bufferedWriter(Charsets.ISO_8859_1,4_000)
 
 
-    for (key in wordsMap.keys) {
-        //println("$key : ${wordsMap.getValue(key)}")
-        writer.write("$key : ${wordsMap.getValue(key)}")
-        writer.newLine()
+    val max = wordsMap.values.maxOf { it }
+    var current = 1
+    val valuesThatExist: MutableList<Int> = mutableListOf()
+    var index = 0
+
+    for (value in wordsMap.values.sorted()) {
+        if (value !in valuesThatExist && value >= current) {
+            valuesThatExist.add(value)
+        }
     }
-    writer.flush()
-    writer.close()
-    */
 
-}
+    while (current <= max) {
 
-fun makeOrderedMap(unorderedMap:MutableMap<String,Int>): MutableMap<Int,List<String>> {
-    val orderedMap: MutableMap<Int,List<String>> = mutableMapOf()
-    val indexList: MutableList<String> = mutableListOf()
+        val indexList: MutableList<String> = mutableListOf()
 
-    var occurrence = 0 // occurrence means amount of times the word occurs in the file.
-    while (occurrence < unorderedMap.size) {
-
-        for (word in unorderedMap.keys) {
-            if (unorderedMap.getValue(word) == occurrence) {
+        for (word in wordsMap.keys) {
+            val value = wordsMap.getValue(word)
+            if (current in valuesThatExist && value == current) {
                 indexList.add(word)
             }
         }
-        orderedMap[occurrence] = indexList
-        indexList.clear()
-        occurrence++
+
+        if (indexList.isNotEmpty()) {
+            writer.write("\n$current ----- \n${indexList.joinToString("\n")}")
+            writer.flush()
+        }
+
+        if (index == valuesThatExist.size) {
+            break
+        }
+        current = valuesThatExist[index]
+        index++
+
+
     }
-
-    return orderedMap
-
+    writer.close()
 
 }
 
 fun removeSpecials(word: String): String {
     val specials = listOf('!','@','#','$','%','^','&','*','(',')','-','_','+','=',
-        ',','.',':',';','?',']','[','}','{','/','\u0022','\u201c','\u201d', '\u0060')
+        ',','.',':',';','?',']','[','}','{','/','\u0022','\u201c','\u201d', '\u0060','\u000d','\u000a')
     return word.filter {
         it !in specials
     }
