7e39cef33342c10e163ce04d4c1c3c7393371197
Onkornix <dymyers@icloud.com>
2024-04-04 15:30:55 -0700

    making it better

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,9 @@
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.File
-import java.nio.file.Files
-import java.nio.file.Path
 fun main() {
     print("Complete path to input text file: ")
-    var input = "/Users/4JStudent/Documents/text.txt"//readln()
-    while (!isInputFileOK(Path.of(input))) {
-        print("Complete path to input file: ")
-        input = readln()
-    }
+    val input = "/Users/4JStudent/Documents/Paradise.txt"//readln()
     print("Complete path to desired output file: ")
     val output = "/Users/4JStudent/Documents/output.txt"//readln()
     val occurrenceToWordsMap = createWordsMap(input)
@@ -32,59 +26,65 @@ fun main() {
     writer.newLine()
     writer.newLine()
     for (occurrence in occurrenceToWordsMap.keys) {
-        writer.write("$occurrence: (${occurrenceToWordsMap.getValue(occurrence).size})")
-        writer.newLine()
-        writer.write(occurrenceToWordsMap
-            .getValue(occurrence)
-            .sorted()
-            .joinToString("\n"))
-        writer.newLine()
-        writer.newLine()
+        writer.write("$occurrence: \n${occurrenceToWordsMap.getValue(occurrence).joinToString("\n")}\n\n")
         writer.flush()
     }
     writer.close()
     println("Done!")
 }
-fun createWordsMap(inputPath: String) : MutableMap<Int, List<String>>{
+fun createWordsMap(inputPath: String) : MutableMap<Int, List<String>> {
+    val specials = listOf('!','@','#','$','%','^','&','*','(',')','-','_','+','=', ',','.',':',';','?',']','[','}','{','/','\\')
     val bufReader = BufferedReader((File(inputPath).reader()))
-    val wordToOccurrenceMap: MutableMap<String,Int> = mutableMapOf()
-    val specialCharacters = listOf('!',',','.',':',';','?','\\','\u0022','\u0027','\u201c','\u201d', '\u0060','\u000d','\u000a','\u0009', '\u00a0', '\u0020')
+    val wordToOccurrenceMap: MutableMap<String, Int> = mutableMapOf()
     var line: String?
+
     while (
         run {
             line = bufReader.readLine()
             line
-        } != null)  {
-        val lineContents: String = line!!
-        val wordList: MutableList<String> = mutableListOf()
-        val wordBuilder = StringBuilder()
-        for ((index, char) in lineContents.withIndex()) {
-            if (char.isWhitespace() || char in listOf('\u0020', '\n', "")) {
-                wordList.add(wordBuilder
-                    .toString()
-                    .lowercase()
-                    .filter { it !in specialCharacters })
-                wordBuilder.clear()
+        } != null) {
+        val lineIterator = line!!
+            .lowercase()
+            .filter { it !in specials }
+            .split(' ')
+            .iterator()
+        val nextWord = lineIterator.next()
+        if (nextWord in wordToOccurrenceMap.keys) {
+                wordToOccurrenceMap[nextWord] = (wordToOccurrenceMap.getValue(nextWord) + 1)
             }
-            else if (index == lineContents.length - 1) {
-                wordBuilder.append(char)
-                wordList.add(wordBuilder
-                    .toString()
-                    .lowercase()
-                    .filter { it !in specialCharacters })
-                wordBuilder.clear()
-            }
-            else {
-                wordBuilder.append(char)
-            }
-        }
-        for (word in wordList) {
-            if (word.isBlank()) continue
-            if (word in wordToOccurrenceMap.keys) {
-                wordToOccurrenceMap[word] = (wordToOccurrenceMap.getValue(word) + 1)
-            }
-            wordToOccurrenceMap.putIfAbsent(word, 1)
-        }
+            wordToOccurrenceMap.putIfAbsent(nextWord, 1)
+
+
+//        val wordList: MutableList<String> = mutableListOf()
+//        val wordBuilder = StringBuilder()
+//        for (char in lineContents) {
+//            if (char.isWhitespace() || char in listOf('\u0020', '\n', "")) {
+//                wordList.add(wordBuilder
+//                    .toString()
+//                    .lowercase()
+//                    .filter { it !in specials}
+//                )
+//                wordBuilder.clear()
+//            } else if (lineContents.indexOf(char) == lineContents.lastIndex) {
+//                wordBuilder.append(char)
+//                wordList.add(wordBuilder
+//                    .toString()
+//                    .lowercase()
+//                    .filter { it !in specials }
+//                )
+//                wordBuilder.clear()
+//            } else {
+//                wordBuilder.append(char)
+//            }
+//        }
+//        for (word in wordList) {
+//            if (word.isBlank()) continue
+//            if (word in wordToOccurrenceMap.keys) {
+//                wordToOccurrenceMap[word] = (wordToOccurrenceMap.getValue(word) + 1)
+//            }
+//            wordToOccurrenceMap.putIfAbsent(word, 1)
+//
+//        }
     }
     val valuesThatExist: MutableList<Int> = mutableListOf()
     for (number in wordToOccurrenceMap.values.sorted()) {
@@ -111,21 +111,4 @@ fun createWordsMap(inputPath: String) : MutableMap<Int, List<String>>{
     }
     wordToOccurrenceMap.clear()
     return occurrenceToWordsMap
-}
-fun isInputFileOK(path: Path) : Boolean {
-    when {
-        Files.notExists(path) -> {
-            println("File does not exist")
-            return false
-        }
-        !Files.isRegularFile(path) -> {
-            println("File is not a regular file")
-            return false
-        }
-        !Files.isReadable(path) -> {
-            println("File is not readable")
-            return false
-        }
-    }
-    return true
 }
