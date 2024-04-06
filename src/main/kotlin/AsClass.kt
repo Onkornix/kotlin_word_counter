1ce3fe532a370db27f4d6cb64e1446e181e4b179
onkornix <dymyers@icloud.com>
2024-04-05 21:39:49 -0700

    un-minified it. should be 100% now

--- src/main/kotlin/AsClass.kt
+++ src/main/kotlin/AsClass.kt
@@ -1,19 +1,21 @@
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.File
+
 class Worderer {
     private val ungroupedMap = mutableMapOf<String,Int>()
     private val groupedMap = mutableMapOf<Int,MutableList<String>>()
-    private val inputOutputPaths = readln().split(' ')
-    private val inputReader = BufferedReader(File(inputOutputPaths[0]).reader())
-    private val outputWriter = BufferedWriter(File(inputOutputPaths[1]).writer())
+    private val inputAndOutputPaths = readln().split(' ')
+    private val inputReader = BufferedReader(File(inputAndOutputPaths[0]).reader())
+    private val outputWriter = BufferedWriter(File(inputAndOutputPaths[1]).writer())
+
     init {
-        val specials = listOf('!','@','#','$','%','^','&','*','(',')','-','_','+','=', ',','.',':',';','?',']','[','}','{','/','\\')
+        val specialCharacters = listOf('!','@','#','$','%','^','&','*','(',')','-','_','+','=', ',','.',':',';','?',']','[','}','{','/','\\')
         for (line in inputReader.lines().iterator()) {
             if (line != null) {
                 addToUngroupedMap(line
                     .lowercase()
-                    .filter { it !in specials }
+                    .filter { it !in specialCharacters }
                     .split(' ')
                     .iterator())
             }
@@ -21,6 +23,7 @@ class Worderer {
         createGroupedMap()
         writeToOutput()
     }
+
     private fun addToUngroupedMap(line: Iterator<String>) {
         for (word in line) {
             if (word.isBlank()) continue
@@ -30,6 +33,7 @@ class Worderer {
             ungroupedMap.putIfAbsent(word, 1)
         }
     }
+
     private fun createGroupedMap() {
         val valuesThatExist = run {
             val valuesThatExist: MutableList<Int> = mutableListOf()
@@ -40,38 +44,56 @@ class Worderer {
             }
             valuesThatExist
         }
+
         var currentOccurrenceValue = ungroupedMap.values.max() - 1
         var index = valuesThatExist.size
+
         while (currentOccurrenceValue >= ungroupedMap.values.min()) {
-            val wordInGroup: MutableList<String> = mutableListOf()
+            val wordsInGroup: MutableList<String> = mutableListOf()
+
             for (word in ungroupedMap.keys) {
-                if (currentOccurrenceValue in valuesThatExist && ungroupedMap.getValue(word) == currentOccurrenceValue) {
-                    wordInGroup.add(word)
+                if (currentOccurrenceValue in valuesThatExist
+                    && ungroupedMap.getValue(word) == currentOccurrenceValue) {
+                    wordsInGroup.add(word)
                 }
             }
-            if (wordInGroup.isNotEmpty()) groupedMap[currentOccurrenceValue] = wordInGroup
+
+            if (wordsInGroup.isNotEmpty()) {
+                groupedMap[currentOccurrenceValue] = wordsInGroup
+            }
+
             if (index == 0) break
             index--
             currentOccurrenceValue = valuesThatExist[index]
+
             val forRemoval: MutableList<String> = mutableListOf()
             for (word in ungroupedMap.keys) {
-                if (ungroupedMap.getValue(word) > currentOccurrenceValue) forRemoval.add(word)
+                if (ungroupedMap.getValue(word) > currentOccurrenceValue) {
+                    forRemoval.add(word)
+                }
             }
-            for (word in forRemoval) ungroupedMap.remove(word)
+            for (word in forRemoval) {
+                ungroupedMap.remove(word)
+            }
         }
-        ungroupedMap.clear()
     }
+
     private fun writeToOutput() {
-        if (File(inputOutputPaths[1]).exists()) File(inputOutputPaths[1]).delete()
-        File(inputOutputPaths[1]).createNewFile()
+        if (File(inputAndOutputPaths[1]).exists()) File(inputAndOutputPaths[1]).delete()
+        File(inputAndOutputPaths[1]).createNewFile()
+
         for (occurrence in groupedMap.keys) {
-            outputWriter.write("$occurrence: \n${groupedMap.getValue(occurrence).joinToString("\n")}\n\n")
+            outputWriter.write("$occurrence: \n${groupedMap.getValue(occurrence).joinToString("\n")}")
+            outputWriter.newLine()
+            outputWriter.newLine()
             outputWriter.flush()
         }
+
         outputWriter.close()
         println("Done!")
     }
 }
+
 fun main() {
     print("complete paths to input and then output files (separated by a space): ")
     Worderer()
